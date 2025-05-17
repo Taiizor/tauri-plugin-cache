eb6db06f2de888ba4c04a59b14090628b9caf87f
Taiizor <41683699+Taiizor@users.noreply.github.com>
2025-05-17 05:51:24 +0300

    Initial commit

---
+++ src/desktop.rs
@@ -0,0 +1,22 @@
+use serde::de::DeserializeOwned;
+use tauri::{plugin::PluginApi, AppHandle, Runtime};
+
+use crate::models::*;
+
+pub fn init<R: Runtime, C: DeserializeOwned>(
+  app: &AppHandle<R>,
+  _api: PluginApi<R, C>,
+) -> crate::Result<Cache<R>> {
+  Ok(Cache(app.clone()))
+}
+
+/// Access to the cache APIs.
+pub struct Cache<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> Cache<R> {
+  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    Ok(PingResponse {
+      value: payload.value,
+    })
+  }
+}
---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use tauri::{
+  plugin::{Builder, TauriPlugin},
+  Manager, Runtime,
+};
+
+pub use models::*;
+
+#[cfg(desktop)]
+mod desktop;
+#[cfg(mobile)]
+mod mobile;
+
+mod commands;
+mod error;
+mod models;
+
+pub use error::{Error, Result};
+
+#[cfg(desktop)]
+use desktop::Cache;
+#[cfg(mobile)]
+use mobile::Cache;
+
+/// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the cache APIs.
+pub trait CacheExt<R: Runtime> {
+  fn cache(&self) -> &Cache<R>;
+}
+
+impl<R: Runtime, T: Manager<R>> crate::CacheExt<R> for T {
+  fn cache(&self) -> &Cache<R> {
+    self.state::<Cache<R>>().inner()
+  }
+}
+
+/// Initializes the plugin.
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+  Builder::new("cache")
+    .invoke_handler(tauri::generate_handler![commands::ping])
+    .setup(|app, api| {
+      #[cfg(mobile)]
+      let cache = mobile::init(app, api)?;
+      #[cfg(desktop)]
+      let cache = desktop::init(app, api)?;
+      app.manage(cache);
+      Ok(())
+    })
+    .build()
+}
