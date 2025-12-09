e8d769c0d167938cba572f21536b319ea8806d7d
Taiizor <41683699+Taiizor@users.noreply.github.com>
2025-12-09 06:55:09 +0300

    Refactor set method to support arbitrary JSON values
    Replaces the generic SetRequest with a non-generic SetRequestArgs using AnyCodableValue to allow storing any JSON-encodable value. Updates the set method to use the new argument parsing and encoding logic, improving flexibility and simplifying argument handling.

--- ios/Sources/CachePlugin.swift
+++ ios/Sources/CachePlugin.swift
@@ -36,10 +36,65 @@ class CompressionConfig: Decodable {
     let method: String
 }
 
-class SetRequest<T: Decodable>: Decodable {
+// Non-generic version for parsing set arguments
+class SetRequestArgs: Decodable {
     let key: String
-    let value: T
+    let value: AnyCodableValue
     let options: SetItemOptions?
+}
+
+// Helper to handle any JSON value
+struct AnyCodableValue: Codable {
+    let value: Any
+    
+    init(_ value: Any) {
+        self.value = value
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if container.decodeNil() {
+            value = NSNull()
+        } else if let bool = try? container.decode(Bool.self) {
+            value = bool
+        } else if let int = try? container.decode(Int.self) {
+            value = int
+        } else if let double = try? container.decode(Double.self) {
+            value = double
+        } else if let string = try? container.decode(String.self) {
+            value = string
+        } else if let array = try? container.decode([AnyCodableValue].self) {
+            value = array.map { $0.value }
+        } else if let dict = try? container.decode([String: AnyCodableValue].self) {
+            value = dict.mapValues { $0.value }
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode value")
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        
+        switch value {
+        case is NSNull:
+            try container.encodeNil()
+        case let bool as Bool:
+            try container.encode(bool)
+        case let int as Int:
+            try container.encode(int)
+        case let double as Double:
+            try container.encode(double)
+        case let string as String:
+            try container.encode(string)
+        case let array as [Any]:
+            try container.encode(array.map { AnyCodableValue($0) })
+        case let dict as [String: Any]:
+            try container.encode(dict.mapValues { AnyCodableValue($0) })
+        default:
+            throw EncodingError.invalidValue(value, EncodingError.Context(codingPath: container.codingPath, debugDescription: "Cannot encode value"))
+        }
+    }
 }
 
 class SetItemOptions: Decodable {
@@ -163,40 +218,27 @@ class CachePlugin: Plugin {
     }
     
     @objc public func set(_ invoke: Invoke) throws {
-        // A slightly different approach to decode generic types in Swift
-        let json = invoke.argsJson
-        guard let jsonData = json.data(using: .utf8) else {
-            throw CachePluginError.invalidArgs("Invalid JSON data")
-        }
+        // Parse arguments using the proper Tauri API
+        let args = try invoke.parseArgs(SetRequestArgs.self)
+        let key = args.key
         
-        guard let dict = try JSONSerialization.jsonObject(with: jsonData) as? [String: Any],
-              let key = dict["key"] as? String,
-              dict["value"] != nil else {
-            throw CachePluginError.invalidArgs("Missing key or value")
-        }
-        
-        // Extract options separately
+        // Extract options
         var ttl: TimeInterval? = nil
         var shouldCompress = self.defaultCompression
         var compressionMethodToUse = self.compressionMethod
         
-        if let options = dict["options"] as? [String: Any] {
-            ttl = options["ttl"] as? TimeInterval
-            if let compress = options["compress"] as? Bool {
+        if let options = args.options {
+            ttl = options.ttl
+            if let compress = options.compress {
                 shouldCompress = compress
             }
-            if let method = options["compressionMethod"] as? String {
+            if let method = options.compressionMethod {
                 compressionMethodToUse = method
             }
         }
         
         // Store as JSON data
-        let valueData: Data
-        if let value = dict["value"] {
-            valueData = try JSONSerialization.data(withJSONObject: value)
-        } else {
-            valueData = "null".data(using: .utf8)!
-        }
+        let valueData = try JSONEncoder().encode(args.value)
         
         // Apply compression
         let finalData: Data
